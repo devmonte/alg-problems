0264ee7b7723e8a17ee7884ad8037144eec77dab
monte <greg.jonca@gmail.com>
2019-04-13 15:33:14 +0200

    Add example: find most frequent char in string

---
+++ Strings/ExerciseOne.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    //Find most popular character in given string
+    public class ExerciseOne
+    {
+        public char FindMostFrequentCharacterInString(string input)
+        {
+            var inputArray = input.ToArray();
+            var charDict = new Dictionary<char, int>();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (charDict.Any(k => k.Key == inputArray[i]))
+                    charDict[inputArray[i]]++;
+                else
+                    charDict.Add(inputArray[i], 1);
+            }
+
+            int max = 0;
+            char found = '0';
+            foreach (var item in charDict.Keys)
+            {
+                if (max < charDict[item])
+                {
+                    max = charDict[item];
+                    found = item;
+                }
+            }
+            return found;
+        }
+
+        public char FindMostFrequentCharacterInStringWithLINQ(string input)
+        {
+            var t = input.GroupBy(s => s).OrderByDescending(x => x.Count()).First().Key;
+            return t;
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new ExerciseOne().FindMostFrequentCharacterInString("TeeeestStringSomeInput"));
+            Console.WriteLine(new ExerciseOne().FindMostFrequentCharacterInStringWithLINQ("TeeeestStringSomeInput"));
+            Console.ReadKey();
+        }
+    }
+}
